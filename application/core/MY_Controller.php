3631ce4b7124bc2453eb019e2f420ef1923d94f5
rabbitzhang52 <rabbitzhang52@gmail.com>
2012-03-28 05:18:52 +0000

    部署

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * class: My_Controller
+ * filename: MY_Controller.php
+ * description :继承于CI_Controller 重写_remap() 方法，实现在url中隐藏控制器中index方法
+ * author :zhanghui rabbitzhang52@gmail.com
+ * create :2012年3月26日19:34:32
+ */
 	class MY_Controller extends CI_Controller {
 		function __construct() {
 			parent::__construct();
