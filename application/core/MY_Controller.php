4502d757e5091ea4d09258e8a100e2a0c67a2c93
rabbitzhang52@gmail.com <rabbitzhang52@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-04-06 04:23:13 +0000

    修改很多~

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -28,13 +28,24 @@ require_once APPPATH . 'libraries/access.php';
 			}
 		}
 		
+		function _require_login() {
+			if($this->session->userdata('type') == 'guest') {
+				redirect('index/login');
+			}
+		}
+		
 		function _remap($method, $params = array()) {
 			if (method_exists($this, $method)){
 				return call_user_func_array(array($this, $method), $params);
 			} else {
-				$param = array($method);
-				$params = array_merge($param, $params);
+				array_unshift($params, $method);
 				return call_user_func_array(array($this, 'index'), $params);
 			}
-		}	 
+		}
+		
+		function _require_ajax() {
+			if(!$this->input->post('ajax')) {
+				show_error('未定义操作');
+			}
+		}
 	}
