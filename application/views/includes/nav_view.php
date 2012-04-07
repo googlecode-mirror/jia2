322577e56fe93059704bf081aef7e7a34ae6cffd
tiramisu101111@gmail.com <tiramisu101111@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-04-07 10:28:52 +0000

    修改nav冲突

--- application/views/includes/nav_view.php
+++ application/views/includes/nav_view.php
@@ -1,19 +1,19 @@
 <div class="header">
 	<div class="head">
 		<div class="left">
-			<?=anchor('', '<strong>Jia2网Logo</strong>', 'class="nav_1"') ?>
-			<?=anchor('', '首页', 'class="nav_1"') ?><?=anchor('personal', '个人主页', 'clas="nav_1"') . anchor('', '搜索社团', 'class="nav_1"')?>
+			<?=anchor('', '<strong>Jia2网Logo</strong>', 'class="logo"') ?>
+			<?=anchor('', '首页', '') ?><?=anchor('personal', '个人主页', '') . anchor('', '搜索社团', '')?>
 		</div>
 		<div class="right">
 			<?=form_open('') ?>
 				<?=form_input(array('id' => 'textfield', 'maxlength' => 50, 'class' => 'keywords', 'name' => 'keywords')) ?>
 				<?=form_close() ?>
 			<? if($this->session->userdata('type') == 'register'): ?>
-				<?=anchor('', '站内信', 'class="nav_1"') ?>
-				<?=anchor('', '好友请求', 'class="nav_1"') ?>
-				<?=anchor('', '通知', 'class="nav_1"') ?>
+				<?=anchor('', '站内信', '') ?>
+				<?=anchor('', '好友请求', '') ?>
+				<?=anchor('', '通知', '') ?>
 				<?=anchor('', '账户', 'id="account-drop-down"') ?>
-				<?=anchor('personal/setting', '设置', 'class="nav_1"') ?>
+				<?=anchor('personal/setting', '设置', '') ?>
 				<?=anchor('index/logout', '退出') ?>
 			<? else: ?>
 				<?=anchor('index/login', '登录') ?>
