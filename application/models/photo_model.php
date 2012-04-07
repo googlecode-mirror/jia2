6e1286fe57c1748bbead5b750ef0413da36e2305
rabbitzhang52 <rabbitzhang52@gmail.com>
2012-04-07 10:20:14 +0000

    文件夹结构

--- application/models/photo_model.php
+++ application/models/photo_model.php
@@ -2,7 +2,29 @@
 	class Photo_model extends CI_Model {
 		function __construct() {
 			parent::__construct();
+			$this->load->library('image_lib');
 		}
 		
+		function upload($mode = 'avatar') {
+			$config = array(
+			  		'allowed_types' => 'jpeg|jpg|png|gif',
+			  		'upload_path' => $this->upload_path,
+			  		'max_size' => '2048',
+			  		'overwrite' => TRUE,
+			  		'file_name'	=> 'avatar_id' . $user_id
+			  	);
+			switch ($mode) {
+				case 'avatar':
+					
+					break;
+			}
+		}
 		
+		function avatar() {
+			
+		}
+		
+		function create_thumb() {
+			
+		}
 	}
