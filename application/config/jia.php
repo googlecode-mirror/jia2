09c0b6be949ee2a9ccd46ebcbde2b82dd0b73e1a
rabbitzhang52@gmail.com <rabbitzhang52@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-04-07 10:20:14 +0000

    文件夹结构

--- application/config/jia.php
+++ application/config/jia.php
@@ -7,3 +7,11 @@
 	$config['post_type_forward'] = 2;
 	// 每页显示的信息数量
 	$config['page_size'] = 20;
+	// 个人头像上传相对路径
+	$config['personal_avatar_path'] = 'data/personal/avatar';
+	// 个人照片上传路径
+	$config['personal_album_path'] = 'data/personal/album';
+	// 社团头像上传路劲
+	$config['corporation_avatar_path'] = 'data/corporation/avatar';
+	// 社团照片上传路径
+	$config['corporation_album_path'] = 'data/corporation/avatar';
