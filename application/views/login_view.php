1e362b52963a96cf2fd1332a18e269eecb4c8f59
tiramisu101111@gmail.com <tiramisu101111@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-04-08 08:47:10 +0000

    修改过一些class的命名，还有样式表的整理

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,29 +1,31 @@
-<div class="bg">
+	<div class="bg">
 			<div class="login">
 				<div class="title">
 					Jia2网logo_<span>登录</span>
 				</div>
-				<div class="form_bg"></div>
 				<?=form_open('index/do_login') ?>
 					<div class="field">
-						<label for="email">邮&nbsp; 　箱:</label>
+						<label for="email">邮&nbsp; 　箱：</label>
 						<input type="text" name="email" id="email" maxlength="32" tabindex="1" />
-						<span class="prompt" id="email_prompt"></span>
+						<span class="prompt" id="email_prompt">请输入正确的邮箱</span>
 					</div>
 					<div class="field">
-						<label>密&nbsp; 　码:</label>
+						<label>密&nbsp; 　码：</label>
 						<span>
 							<input type="password" name="pass" id="pass" maxlength="20" tabindex="2" />
 						</span>
 						<span class="prompt" id="pass_prompt"></span>
 					</div>
-					<div class="checkbox"></div>
-					<div class="submit">
+					
+					<div class="field_below remmber">
+						<input type="checkbox" name="remmber" value="1"><span>记住我</span>
+					</div>
+					<div class="field_below">
 						<?=form_submit('submit', '登录', 'class="button"') ?>
 						<span>|</span><span><?=anchor(site_url('index/regist'), '用户注册') ?> </span>
-						<span><input type="checkbox" name="remmber" value="1"><span>记住我</span></span>
+						<span>|</span><span><?=anchor(site_url('index/_guest'), '游客登录') ?> </span>
 					</div>
 				<?=form_close() ?>
 			</div>
 		</div>
-		</div>
+	
--- application/views/regist_view.php
+++ application/views/regist_view.php
@@ -1,28 +1,27 @@
 <div class="bg">
 	<div class="login register">
-    <div class="title title_r">Jia2网logo_<span>注册</span></div>
-    <div class="form_bg form_bg_r"></div>
+    <div class="title">Jia2网logo_<span>注册</span></div>
     <?=form_open('index/do_regist', 'id = "reg"') ?>
     	<div class="field">
             <label >邮&nbsp; 　箱：</label>
             <input type="text" name="email" id="email" value="" maxlength="32" tabindex="1" />
-            <span class="prompt" class="password-edit"></span>
+            <span class="prompt" id="email_prompt">请输入正确的邮箱</span>
         </div>
         <div class="field">
             <label>姓&nbsp; 　名：</label>
             <span >
              <input type="text" name="name" id="name" maxlength="20" tabindex="2" />
             </span>
-            <span class="prompt" class="password-edit"></span>            
+            <span class="prompt" id="name_prompt">用户名长度不超过20个字符</span>            
          </div>
          <div class="field">
             <label >密&nbsp; 　码：</label>
             <span >
              <input type="password" name="pass" id="pass" maxlength="20" tabindex="2" />
             </span>
-            <span class="prompt" class="password-edit"></span>       
+            <span class="prompt" id="pass_prompt">密码要求什么的</span>       
          </div>
-         <div class="submit">
+         <div class="field_below">
          	<?=anchor('', '同意以下协议并') ?>
             <?=form_submit('submit', '注册', 'class="button"') ?>
          </div>  
