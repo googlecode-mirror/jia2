2090760e1edfcf5aba3f0487d92bb02cd04b371d
rabbitzhang52@gmail.com <rabbitzhang52@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-04-25 13:29:27 +0000

    整合视图，好几把累。。。

--- application/views/post/user_posts_view.php
+++ application/views/post/user_posts_view.php
@@ -1,41 +1,3 @@
-<!--
-<div id="cc01">
-	<? if(array_key_exists('personal', $posts)):?>
-	<? foreach ($posts['personal'] as $post):?>
-	<div class="article_one">
-		<?=anchor('personal/profile/' . $post['user'][0]['id'], '<img src="'. avatar_url($post['user'][0]['avatar']) .'">', 'class="head_pic"') ?>
-		<div class="article_sub_box">
-			<h4><a href="<?=site_url('personal/profile/' . $post['user'][0]['id']) ?>"><?=$post['user'][0]['name']?></a>&nbsp;<?=$post['content']?></h4>
-			<p>
-				2分钟前 <a href="#">收起回复</a> | <a href="#">分享</a>
-			</p>
-			<div class="comment">
-				<ul>
-			<? if(array_key_exists('comment', $post)):?>
-			<? foreach($post['comment'] as $comment):?>
-					<li>
-						<?=anchor('personal/profile/' . $comment['user'][0]['id'], '<img src="'. avatar_url($comment['user'][0]['avatar']) .'" >','class="head_pic"') ?>
-						<p><?=anchor('personal/profile/' . $comment['user'][0]['id'], $comment['user'][0]['name']) ?>：<?=$comment['content']?><a href="#" class="reply"'>回复</a><br />
-						<small>2012-04-09 20:43</small></p>
-					</li>
-				
-			<? endforeach?>
-			<? endif?>
-				</ul>
-				<p>
-					<?=form_textarea(array('name' => 'comment_content', 'post_id' => $post['id'], 'type_id' =>$post['type_id'] , 'owner_id' => $post['owner_id'], 'cols' => 60, 'rows' => 2))
-					?>
-				</p>
-				<p>
-					<?=form_button('comment', '评论','class="button"')?>
-				</p>
-			</div>
-		</div>
-	</div>
-	<? endforeach?>
-	<? endif?>
-</div>
--->
 <? if(array_key_exists('personal', $posts)):?>
 <? foreach ($posts['personal'] as $post):?>
 <li class="feed_a">
@@ -73,6 +35,10 @@
 					<? endforeach?>
 					<? endif?>
 			</ul>
+			<div>
+				<?=form_textarea('content') ?>
+				<?=form_submit('submit', '评论') ?>
+			</div>
 		</div>
 </li>				
 <? endforeach ?>
