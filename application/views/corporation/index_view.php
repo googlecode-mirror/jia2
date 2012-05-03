9efd38bca8fd1358582e009db348e881f3e701d8
rabbitzhang52@gmail.com <rabbitzhang52@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-05-03 11:46:52 +0000

    社团之家首页视图更名

---
+++ application/views/corporation/index_view.php
@@ -0,0 +1,46 @@
+<div id="main">
+	<a href="<?=site_url('corporation/add') ?>" class="creat_button button"><i>+</i> 创建社团</a>
+	<div id="search_box">
+		<div id="search-bar">
+			<input type="text" name="keywords" class="serch_input" value="动漫/体育/科技/社科..."/>
+			<input type="buttom" name="search" class="serch_button" value="搜索" />
+		</div>
+	</div>
+	<div id="search_item">
+			<ul>
+				<li class="sd01" id="01">
+					<a href="#" id="active">我的社团&nbsp;(2)</a>
+				</li>
+				<li class="sd02" id="02">
+					<a href="#">我关注的社团&nbsp;(6)</a>
+				</li>
+			</ul>
+	</div>
+	<div id="feeds_container" class="feeds">
+	<ul id="feed_1">
+		<li class="feed_a">
+			<div class="img_block"><img src="images/head_pic/user02.jpg" /></div>
+			<div class="feed_main">
+				<h3 class="asso_name"><a href="association-pro.html">启明拓展协会</a><span>日志(6)</span><span>相册(6)</span><span>说说(20)</span></h3>
+				<ul class="asso_ul">
+					<li><a href="">启明拓展协会最新活动安排</a></li>
+					<li><a href="">启明拓展协会的活动日志</a></li>
+					<li><a href="">感谢群众还记得我</a></li>
+				</ul>
+			</div>
+		</li>
+		<li class="feed_a">
+			<div class="img_block"><img src="images/head_pic/user02.jpg" /></div>
+			<div class="feed_main">
+				<h3 class="asso_name"><a href="association-pro.html">启明拓展协会</a><span>日志(6)</span><span>相册(6)</span><span>说说(20)</span></h3>
+				<ul class="asso_ul">
+					<li><a href="">启明拓展协会最新活动安排</a></li>
+					<li><a href="">启明拓展协会的活动日志</a></li>
+					<li><a href="">感谢群众还记得我</a></li>
+				</ul>
+			</div>
+		</li>
+		
+	</ul>
+	</div>	
+</div>
