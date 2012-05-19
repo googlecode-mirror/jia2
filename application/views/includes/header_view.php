b9bbbc6389c0799862ab8d43fdba197955079f17
rabbitzhang52 <rabbitzhang52@gmail.com>
2012-05-19 08:34:07 +0000

    资源文件路径

--- application/views/includes/header_view.php
+++ application/views/includes/header_view.php
@@ -5,12 +5,12 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <META HTTP-EQUIV="pragma" CONTENT="no-cache">
         <title><?=(empty($title) ? '未定义标题' : $title) . ' | 加加社团' ?></title>
-        <link rel="stylesheet" type="text/css" href="<?=site_url('resource/css/home.css') ?>" />
-        <link rel="stylesheet" type="text/css" href="<?=site_url('resource/css/main_content.css') ?>" />
-	    <script type="text/javascript" src="<?=site_url('resource/js/jquery.js') ?>"></script>
-	    <script type="text/javascript" src="<?=site_url('resource/js/global.js') ?>"></script>
-	    <script type="text/javascript" src="<?=site_url('resource/js/tab.js') ?>"></script>
-	    <script type="text/javascript" src="<?=site_url('resource/js/inline_window.js') ?>"></script>
+        <link rel="stylesheet" type="text/css" href="<?=base_url('resource/css/home.css') ?>" />
+        <link rel="stylesheet" type="text/css" href="<?=base_url('resource/css/main_content.css') ?>" />
+	    <script type="text/javascript" src="<?=base_url('resource/js/jquery.js') ?>"></script>
+	    <script type="text/javascript" src="<?=base_url('resource/js/global.js') ?>"></script>
+	    <script type="text/javascript" src="<?=base_url('resource/js/tab.js') ?>"></script>
+	    <script type="text/javascript" src="<?=base_url('resource/js/inline_window.js') ?>"></script>
 		<script type="text/javascript">
         	SITE_URL = "<?=site_url() ?>";
         </script>
