3537f6596d50d399225199fe48b9c003381c5c09
tiramisu101111@gmail.com <tiramisu101111@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-06-04 03:06:23 +0000

    按条件搜索样式。。

--- application/views/search_view.php
+++ application/views/search_view.php
@@ -1,37 +1,21 @@
 <script>
-	window.onload=function(){
-		return this.each(function(){
-			if($(this).hasClass('jqTransformHidden')) {return;}
-
-			var $input = $(this);
-			var inputSelf = this;
-
-			//set the click on the label
-			var oLabel=jqTransformGetLabel($input);
-			oLabel && oLabel.click(function(){aLink.trigger('click');});
-			
-			var aLink = $('<a href="#" class="jqTransformCheckbox"></a>');
-			//wrap and add the link
-			$input.addClass('jqTransformHidden').wrap('<span class="jqTransformCheckboxWrapper"></span>').parent().prepend(aLink);
-			//on change, change the class of the link
-			$input.change(function(){
-				this.checked && aLink.addClass('jqTransformChecked') || aLink.removeClass('jqTransformChecked');
-				return true;
-			});
-			// Click Handler, trigger the click and change event on the input
-			aLink.click(function(){
-				//do nothing if the original input is disabled
-				if($input.attr('disabled')){return false;}
-				//trigger the envents on the input object
-				$input.trigger('click').trigger("change");	
-				return false;
-			});
-
-			// set the default state
-			this.checked && aLink.addClass('jqTransformChecked');		
-		});
-	}
-</script>
+$(function(){
+	$(".Checked").toggle(function(){
+			$(this).removeClass("Checked").addClass("Checkbox");
+			$(this).children().removeAttr('checked');
+		},function(){
+			$(this).removeClass("Checkbox").addClass("Checked");
+			$(this).children().attr("checked", "checked");
+		})
+	$(".Checkbox").toggle(function(){
+			$(this).removeClass("Checkbox").addClass("Checked");
+			$(this).children().attr("checked", "checked");
+		},function(){
+			$(this).removeClass("Checked").addClass("Checkbox");
+			$(this).children().removeAttr('checked');
+		})
+})
+</script> 
 <div id="main">
 	<h3>&nbsp;搜索&nbsp;<span id="searh_key">“<?=trim($this->input->post('keywords')) ?>”</span></h3>
 	<div id="search_box">
@@ -43,10 +27,21 @@
 		</div>
 	</div>
 	<p id="chose_box">
-		<div class="rowElem"><label>Checkbox: </label><input type="checkbox" name="chbox" id=""></div>
-		<?=form_checkbox('user', 1, 'checked:checked') ?>用户
+		<span class="CheckboxWrapper Checked">
+			<input type="checkbox" name="chbox" class="chbox" checked="checked"/>
+		</span>
+		<span class="Checkitem">用户</span>
+		<span class="CheckboxWrapper Checkbox">
+			<input type="checkbox" name="chbox" class="chbox"/>
+		</span>
+		<span class="Checkitem">社团</span>
+		<span class="CheckboxWrapper Checkbox">
+			<input type="checkbox" name="chbox" class="chbox"/>
+		</span>
+		<span class="Checkitem">活动</span>
+		<!-- <?=form_checkbox('user', 1, 'checked:checked') ?>用户
 		<?=form_checkbox('corporation', 1) ?>社团
-		<?=form_checkbox('activity', 1) ?>活动
+		<?=form_checkbox('activity', 1) ?>活动 -->
 	</p>
 		<?=form_close() ?>
 	<div class="search_item">
