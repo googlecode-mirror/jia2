299e5356f79aebcb8b161b2fcc39e753e1dcb7d9
rabbitzhang52@gmail.com <rabbitzhang52@gmail.com@ea4caac7-0005-2e3f-1ef5-76329e6fee95>
2012-06-11 08:36:26 +0000

    修复权限验证bug 更新数据库 依旧完善日志

--- resource/js/login_view.js
+++ resource/js/login_view.js
@@ -3,6 +3,9 @@ $(function() {
                 email = $("input[name='email']").val();
                 pass = $("input[name='pass']").val();
                 remember = $("input[name='remember']").val();
+                $submit = $(this);
+                $submit.attr('disabled', 'disabled');
+                $submit.val('正在登录');
                 $.post(SITE_URL+"index/do_login", {
                     ajax:1,
                     email:email,
@@ -10,6 +13,7 @@ $(function() {
                     remember:remember
                 }, function(data) {
                     if(data.verify == 1) {
+                    	$submit.val('登录成功');
                     	href = window.location.href;
                     	if(href.indexOf("?jump=") > 0) {
                     		window.location.href = SITE_URL + href.substr(href.indexOf("?jump=") + 6);
@@ -19,18 +23,12 @@ $(function() {
                     } else {
                         $("#email_prompt").text(data.email);
                         $("#pass_prompt").text(data.pass);
+                        $submit.removeAttr('disabled');
+                        $submit.val('登录');
                         return false;
                     }
                    }, 'json'
                 );
                 return false;
         });
-        $("input[name='submit']").ajaxStart(function() {
-			$(this).val('正在登录');
-			$(this).attr('disabled', 'disabled');
-        });
-        $("input[name='submit']").ajaxComplete(function() {
-        	$(this).val('登录');
-        	$(this).removeAttr('disabled');
-        })
 });
